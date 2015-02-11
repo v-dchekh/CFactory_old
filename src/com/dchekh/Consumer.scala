9b2c0095e459f235275cdc88d31aa5b4e13f5e44
v-dchekh <rrrttt222@tut.by>
2015-02-11 06:34:13 -0300

    Real kafka consumers added.

--- src/com/dchekh/Consumer.scala
+++ src/com/dchekh/Consumer.scala
@@ -1,12 +1,75 @@
 package com.dchekh
 
 import java.util.concurrent.CountDownLatch;
+import java.util.Properties
+import kafka.message._
+import kafka.serializer._
+import kafka.utils._
+import java.util.Properties
+import kafka.utils.Logging
+import scala.collection.JavaConversions._
+import kafka.consumer.ConsumerConfig
+import kafka.consumer.Whitelist
+import kafka.consumer.Consumer
 
-  class Consumer[T](mes: String, sleeptime: Int, cdl : CountDownLatch ) extends Runnable {
-    def run() {
-      Thread.sleep(sleeptime)
-      println(mes)
-      cdl.countDown()
+class Consumer[T](mes: String, sleeptime: Int, cdl: CountDownLatch, cg_config: Properties) extends Runnable {
 
+  val config = new ConsumerConfig(cg_config)
+  val connector = Consumer.create(config)
+  val filterSpec = new Whitelist(cg_config.getProperty("topic"))
+
+  val stream = connector.createMessageStreamsByFilter(filterSpec, 1).get(0)
+  var list = List[String]()
+
+  def run() {
+
+    //    println("topic" + cg_config.getProperty("topic"))
+
+    var message: List[String] = List()
+    while (true) {
+      read(bytes => {
+        message = bytes
+//        println("scala  > received " + message)
+
+      })
+    }
+    //    Thread.sleep(sleeptime)
+    println(mes)
+    cdl.countDown()
+
+  }
+  def read(write: List[String] => Unit) = {
+    // def read  : List[String] = {
+    //    info("reading on stream now")
+
+    var numMessages: Int = 0
+    for (messageAndTopic <- stream) //    while (!stream.isEmpty)
+    {
+      try {
+        var m = messageAndTopic.message()
+        var part = messageAndTopic.partition
+        list = list ++ List(new String(m))
+        numMessages += 1
+        println(messageAndTopic.offset.toString + " : " + new String(m) + "; Partition - " + part+ "; thread - " +mes)
+
+        if (numMessages == 3) {
+          numMessages = 0
+          write(list)
+          list = List()
+        }
+      } catch {
+        case e: Throwable =>
+          if (true) { //this is objective even how to conditionalize on it
+            error("Error processing message, skipping this message: " + e)
+          } else {
+            throw e
+          }
+      }
     }
   }
+
+  def close() {
+    connector.shutdown()
+  }
+
+}
--- src/com/dchekh/ConsumerGroup.scala
+++ src/com/dchekh/ConsumerGroup.scala
@@ -10,31 +10,41 @@ import scala.collection.JavaConversions._
 import kafka.utils.Logging
 import java.util.concurrent.CountDownLatch;
 
+class ConsumerGroup(threadNumber: Int = 5, a_zookeeper: String, a_groupId: String, topic: String, latch: CountDownLatch)
 
-class ConsumerGroup(threadNumber: Int = 5, a_zookeeper: String, a_groupId: String, topic: String, latch : CountDownLatch) 
-//extends Logging 
-{
+  extends Logging {
 
-//  info("setup:start topic=%s for zk=%s and groupId=%s".format(topic, a_zookeeper, a_groupId))
-  
-  def createConsumerConfig(a_zookeeper: String, a_groupId: String): Properties = {
+  //  info("setup:start topic=%s for zk=%s and groupId=%s".format(topic, a_zookeeper, a_groupId))
+
+  def createConsumerConfig(a_zookeeper: String, a_groupId: String, topic: String): Properties = {
     val props = new Properties()
     props.put("zookeeper.connect", a_zookeeper)
     props.put("group.id", a_groupId)
-    props.put("zookeeper.session.timeout.ms", "400")
+    props.put("zookeeper.session.timeout.ms", "4000")
     props.put("zookeeper.sync.time.ms", "200")
     props.put("auto.commit.interval.ms", "1000")
+    props.put("topic", topic)
     props
   }
 
   def launch {
-    var cg_config = createConsumerConfig(a_zookeeper, a_groupId)
+    var cg_config = createConsumerConfig(a_zookeeper, a_groupId, topic)
     println(cg_config)
 
-    for (i <-  1 to threadNumber by 1) {
-      println("start Thread " +a_groupId +" : " + i)
-      var consumer = new Consumer[String]("Thread.sleep " +a_groupId +" : " + i, i * 1000, latch)
-      new Thread(consumer).start()
+    for (i <- 1 to threadNumber by 1) {
+      println("start Thread ****  groupId : " + a_groupId + ", thread : " + i)
+//      Thread.sleep(1)
+      try {
+        var consumer = new Consumer[String]("Thread :" + i, i * 1000, latch, cg_config)
+        new Thread(consumer).start()
+      } catch {
+        case e: Throwable =>
+          if (true) { //this is objective even how to conditionalize on it
+            error("Error creating new Consumer: ", e)
+          } else {
+            throw e
+          }
+      }
 
     }
   }
